b6d57fe28b9196887fe9b249a2a79f60e8c1d02e
082_Farrel Emerson <FarrelEmerson02@gmail.com>
2026-08-11 17:24:18 +0700

    Update solution for Week 2

--- QueueExercise/Program.cs
+++ QueueExercise/Program.cs
@@ -1,6 +1,55 @@
-﻿Queue<string> queue = new Queue<string>();
+﻿/*WEEK 1*/
+// Queue<string> queue = new Queue<string>();
+// string? inputs;
+// Console.WriteLine("Enter commands Enqueue(item) or Process(), separated by ';' or ',' (Ctrl+C to exit):");
+
+// while ((inputs = Console.ReadLine()) != null)
+// {
+//     if (inputs.Contains(";"))
+//     {
+//         String[] inputsArray = inputs.Split(';');
+//         foreach (string input in inputsArray)
+//         {
+//             QueueManager(queue, input);
+//         }
+//     }
+//     else
+//     {
+//         String[] inputArray = inputs.Split(',');
+//         foreach (string input in inputArray)
+//         {
+//             QueueManager(queue, input);
+//         }
+//     }
+// }
+
+// static void QueueManager(Queue<string> queue, string input)
+// {
+//     if (input.Contains("Enqueue("))
+//     {
+//         string item = input.Substring(input.IndexOf('(') + 1, input.IndexOf(')') - input.IndexOf('(') - 1);
+//         queue.Enqueue(item);
+//         Console.WriteLine($"Queued {item}");
+//     }
+//     else if (input.Contains("Process"))
+//     {
+//         if (queue.Count > 0)
+//         {
+//             Console.WriteLine($"Processed {queue.Dequeue()}");
+//         }
+//         else
+//         {
+//             Console.WriteLine("Queue is empty");
+//         }
+//     }
+// }
+
+
+/* WEEK 2 */
+Queue<string> queue = new Queue<string>();
+Queue<string> vipQueue = new Queue<string>();
 string? inputs;
-Console.WriteLine("Enter commands Enqueue(item) or Process(), separated by ';' or ',' (Ctrl+C to exit):");
+Console.WriteLine("Enter commands Enqueue(item)/EnqueueVip() or Process(), separated by ';' or ',' (Ctrl+C to exit):");
 
 while ((inputs = Console.ReadLine()) != null)
 {
@@ -9,7 +58,7 @@ while ((inputs = Console.ReadLine()) != null)
         String[] inputsArray = inputs.Split(';');
         foreach (string input in inputsArray)
         {
-            QueueManager(queue, input);
+            QueueManager(vipQueue, queue, input);
         }
     }
     else
@@ -17,22 +66,32 @@ while ((inputs = Console.ReadLine()) != null)
         String[] inputArray = inputs.Split(',');
         foreach (string input in inputArray)
         {
-            QueueManager(queue, input);
+            QueueManager(vipQueue, queue, input);
         }
     }
 }
 
-static void QueueManager(Queue<string> queue, string input)
+static void QueueManager(Queue<string> vipQueue, Queue<string> queue, string input)
 {
-    if (input.Contains("Enqueue("))
+    if (input.Contains("Vip("))
     {
-        string item = input.Substring(input.IndexOf('(') + 1, input.IndexOf(')') - input.IndexOf('(') - 1);
+        string item = input.Substring(input.IndexOf('(') + 2, input.IndexOf(')') - input.IndexOf('(') - 3);
+        vipQueue.Enqueue(item);
+        Console.WriteLine($"VIP Queued {item}");
+    }
+    else if (input.Contains("Enqueue("))
+    {
+        string item = input.Substring(input.IndexOf('(') + 2, input.IndexOf(')') - input.IndexOf('(') - 3);
         queue.Enqueue(item);
         Console.WriteLine($"Queued {item}");
     }
     else if (input.Contains("Process"))
     {
-        if (queue.Count > 0)
+        if (vipQueue.Count > 0)
+        {
+            Console.WriteLine($"Processed {vipQueue.Dequeue()}");
+        }
+        else if (queue.Count > 0)
         {
             Console.WriteLine($"Processed {queue.Dequeue()}");
         }
