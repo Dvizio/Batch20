22d42af37869a48c7e200bd888cdb7e27954555d
082_Farrel Emerson <FarrelEmerson02@gmail.com>
2026-08-18 17:07:57 +0700

    update Foobar week 3 solution

--- FooBar/Program.cs
+++ FooBar/Program.cs
@@ -23,6 +23,42 @@
 
 /* WEEK 2 */
 
+// string? inputs;
+// int integer;
+// Console.WriteLine("Enter an integer:");
+// while ((inputs = Console.ReadLine()) != null)
+// {
+//     if (int.TryParse(inputs, out integer))
+//     {
+//         var results = new List<string>();
+
+//         for (int i = 1; i <= integer; i++)
+//         {
+//             string output = "";
+
+//             if (i % 3 == 0)
+//                 output += "foo";
+//             if (i % 5 == 0)
+//                 output += "bar";
+//             if (i % 7 == 0)
+//                 output += "jazz";
+
+//             if (string.IsNullOrEmpty(output))
+//             {
+//                 output = i.ToString();
+//             }
+
+//             results.Add(output);
+//         }
+//         Console.WriteLine(string.Join(", ", results));
+//     }
+//     else
+//     {
+//         Console.WriteLine("INTEGER ONLY!");
+//     }
+// }
+
+/* WEEK 3 */
 string? inputs;
 int integer;
 Console.WriteLine("Enter an integer:");
@@ -38,10 +74,14 @@ while ((inputs = Console.ReadLine()) != null)
 
             if (i % 3 == 0)
                 output += "foo";
+            if (i % 4 == 0)
+                output += "baz";
             if (i % 5 == 0)
                 output += "bar";
             if (i % 7 == 0)
                 output += "jazz";
+            if (i % 9 == 0)
+                output += "huzz";
 
             if (string.IsNullOrEmpty(output))
             {
