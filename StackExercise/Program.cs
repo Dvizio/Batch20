d8602e088b6561b004c4d84c9089d3b0c265a7fd
Farrel Emerson <FarrelEmerson02@gmail.com>
2026-08-19 09:27:59 +0700

    Update solution to Stackexercise week 3

--- StackExercise/Program.cs
+++ StackExercise/Program.cs
@@ -45,7 +45,70 @@
 
 /* WEEK 2*/
 
+// Stack<string> stack = new Stack<string>(3);
+// string? inputs;
+// Console.WriteLine("Enter commands Type(item) or Undo(), separated by ';' or ',' (Ctrl+C to exit):");
+// while ((inputs = Console.ReadLine()) != null)
+// {
+//     if (inputs.Contains(";"))
+//     {
+//         String[] inputsArray = inputs.Split(';');
+//         foreach (string input in inputsArray)
+//         {
+//             StackManager(ref stack, input);
+//         }
+//     }
+//     else
+//     {
+//         String[] inputArray = inputs.Split(',');
+//         foreach (string input in inputArray)
+//         {
+//             StackManager(ref stack, input);
+//         }
+//     }
+// }
+
+// static void StackManager(ref Stack<string> stack, string input)
+// {
+//     if (input.Contains("Type("))
+//     {
+//         string item = input.Substring(input.IndexOf('(') + 2, input.IndexOf(')') - input.IndexOf('(') - 3);
+//         if (stack.Count < 3)
+//         {
+//             stack.Push(item);
+//             Console.WriteLine($"Typed {item}");
+//         }
+//         else
+//         {
+//             Stack<string> newStack = new Stack<string>(3);
+//             for (int i = 1; i >= 0; i--)
+//             {
+//                 newStack.Push(stack.ElementAt(i));
+//             }
+//             newStack.Push(item);
+//             Console.WriteLine($"Dropped bottom, Typed {item}");
+//             stack.Clear();
+//             stack = newStack;
+//         }
+
+//     }
+//     else if (input.Contains("Undo()"))
+//     {
+//         if (stack.Count > 0)
+//         {
+//             Console.WriteLine($"Undid {stack.Pop()}");
+//         }
+//         else
+//         {
+//             Console.WriteLine("Stack is empty");
+//         }
+//     }
+// }
+
+/* WEEK 3 */
+
 Stack<string> stack = new Stack<string>(3);
+Stack<string> history = new Stack<string>(3);
 string? inputs;
 Console.WriteLine("Enter commands Type(item) or Undo(), separated by ';' or ',' (Ctrl+C to exit):");
 while ((inputs = Console.ReadLine()) != null)
@@ -55,7 +118,7 @@ while ((inputs = Console.ReadLine()) != null)
         String[] inputsArray = inputs.Split(';');
         foreach (string input in inputsArray)
         {
-            StackManager(ref stack, input);
+            StackManager(ref stack, input, ref history);
         }
     }
     else
@@ -63,15 +126,16 @@ while ((inputs = Console.ReadLine()) != null)
         String[] inputArray = inputs.Split(',');
         foreach (string input in inputArray)
         {
-            StackManager(ref stack, input);
+            StackManager(ref stack, input, ref history);
         }
     }
 }
 
-static void StackManager(ref Stack<string> stack, string input)
+static void StackManager(ref Stack<string> stack, string input, ref Stack<string> history)
 {
     if (input.Contains("Type("))
     {
+        history.Clear();
         string item = input.Substring(input.IndexOf('(') + 2, input.IndexOf(')') - input.IndexOf('(') - 3);
         if (stack.Count < 3)
         {
@@ -96,11 +160,27 @@ static void StackManager(ref Stack<string> stack, string input)
     {
         if (stack.Count > 0)
         {
-            Console.WriteLine($"Undid {stack.Pop()}");
+            string temp = stack.Pop();
+            Console.WriteLine($"Undid {temp}");
+            history.Push(temp);
         }
         else
         {
             Console.WriteLine("Stack is empty");
         }
     }
+    else if (input.Contains("Redo()"))
+    {
+        if (history.Count() != 0)
+        {
+            stack.Push(history.Pop());
+            Console.WriteLine($"Redid {stack.Peek()}");
+        }
+        else
+        {
+            Console.WriteLine("There's no history between us");
+        }
+
+    }
 }
+
